aa6fa85ac0bbdab94c2794bcc00f4357146b6338
qiyaoda <qiyaoda123@163.com>
2018-11-20 23:26:27 +0800

    init

---
+++ app/controller/home.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.response.body = 'hello world'
+    
+  }
+}
+
+module.exports = HomeController;
